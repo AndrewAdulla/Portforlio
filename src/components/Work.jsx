5b82b685b8f9f0a33fbcc2274c0651c79603e47f
AndrewAdulla <adulladrew@gmail.com>
2022-04-27 17:22:40 +0100

    image

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import WorkImg from '../assets/workImg.jpeg';
+import portfolio from '../assets/portfolio.jpg';
 import realEstate from '../assets/realestate.jpg';
+import WorkImg from '../assets/workImg.jpeg';
 
 const Work = () => {
   return (
@@ -8,7 +9,7 @@ const Work = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
-            Work
+            Projects
           </p>
           <p className='py-6'> Check out some of my recent work whilst at Brunel University University</p>
         </div>
@@ -18,13 +19,13 @@ const Work = () => {
 
             {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${portfolio})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                Porfolio
               </span>
               <div className='pt-8 text-center'>
                 <a href='/'>
@@ -32,7 +33,7 @@ const Work = () => {
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/AndrewAdulla/Portforlio/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
